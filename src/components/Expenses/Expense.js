44531a0514b186956b7febfe8a79969e288097b0
manishSanodiya <122765253+manishSanodiya@users.noreply.github.com>
2023-03-09 10:12:45 +0530

    last

--- src/components/Expenses/Expense.js
+++ src/components/Expenses/Expense.js
@@ -5,6 +5,7 @@ import './Expense.css';
 import React,{ useState } from "react";
 import ExpensesFilter from "./ExpensesFilter";
 import ExpenseList from "./ExpenseList";
+import ExpensesChart from "./ExpensesChart";
 
 
 const Expense = (props) => {
@@ -24,27 +25,8 @@ const Expense = (props) => {
       <div>
         <Card className="expenses">
           <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler}/>
-         
-         {/* <ExpenseItem
-           title={props.item[0].title}
-           amount={props.item[0].amount}
-           date={props.item[0].date}
-         />
-         <ExpenseItem
-           title={props.item[1].title}
-           amount={props.item[1].amount}
-           date={props.item[1].date}
-         />
-         <ExpenseItem
-           title={props.item[2].title}
-           amount={props.item[2].amount}
-           date={props.item[2].date}
-         />
-         <ExpenseItem
-           title={props.item[3].title}
-           amount={props.item[3].amount}
-           date={props.item[3].date}
-         /> */}
+         <ExpensesChart expenses={filteredExpenses}/>
+        
          
          <ExpenseList item ={filteredExpenses}/>
        </Card>
